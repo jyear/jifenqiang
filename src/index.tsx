7c910efde70ebc9bfc74bcfd2eaf0066fce8b381
杨叙 <yx53074@hotmail.com>
2018-11-20 22:53:06 +0800

    22

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDom from "react-dom";
 import { Provider } from "react-redux";
 import { LocaleProvider } from "antd";
+//源码使用的是node模式到处 ts-loader 没导出导致没法加载语言包 所以使用node模式来引入文件
 const zhCN = require("antd/lib/locale-provider/zh_CN");
 
 import createHistory from "history/createHashHistory";
